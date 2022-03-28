0366c957b8780bfd9a65f35123c9987c128319ad
syahshiimi <syahrulanuar@Syahruls-MacBook-Air.local>
2022-03-29 00:19:59 +0800

    updated server js files

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["882b6216688f266d", "aad43806f7be0463"]),
   },
 });
